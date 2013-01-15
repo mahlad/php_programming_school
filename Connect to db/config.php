5e50c9172d39bbae6d42415e608cb7e49e8d61a6
mahlad <mahlad365@gmail.com>
2013-01-15 12:33:16 +0330

    change it to usuall form

--- Connect to db/config.php
+++ Connect to db/config.php
@@ -7,43 +7,32 @@ if(!$config)
 	die(mysql_error());
 else {
 	echo'conected to DB successfuly';
-	/*----craete db----*/
 	
+	/*----craete db-just one time run----*/
 	// if(mysql_query('CREATE DATABASE phpClass'))
-	// 	echo "</br>your db:phpClass created successfuly";
-	// else
-	// 	echo '</br>db:phpClass NOt craeted beacuse of: '.mysql_error();
-	
-	/*----select db----*/
-	// mysql_select_db('phpClass',$config);
-	// $query='CREATE TABLE persons(
-	// 	id int not null auto_increment,
-	// 	primary key(id),
-	// 	firstname varchar(20),
-	// 	lastname varchar(20),
-	// 	age int
-	// 	)';
-	// if(mysql_query($query))
-	// 	echo '</br>"persons" table create successfuly';
-	// else echo '</br>"persons" table not created beacuse of: '.mysql_error();
+	// 		echo "</br>your db:phpClass created successfuly";
+	// 	else
+	// 		echo '</br>db:phpClass NOt craeted beacuse of: '.mysql_error();
 
-	/*----Insert into table----*/
-	// mysql_select_db('phpClass',$config);
-	// if(mysql_query('INSERT INTO persons (firstname,lastname,age) VALUES ("Ali","Ahmadi","20")'))
-	// 	echo '</br> Ali`s information inserted ';
-	// else die(mysql_error());
-	// if(mysql_query('INSERT INTO persons (firstname,lastname,age) VALUES ("Hassan","Ahmadi","28")'))
-	// 	echo '</br> Hassan`s information inserted ';
-	// else die(mysql_error());
+	/*----select db----*/
+	mysql_select_db('phpClass',$config);
+
+	/*----craete table-just one time run----*/ 
+	 // $query='CREATE TABLE persons(
+		// 	id int not null auto_increment,
+		// 	primary key(id),
+		// 	firstname varchar(20),
+		// 	lastname varchar(20),
+		// 	age int
+		// 	)';
+		// if(mysql_query($query))
+		// 	echo '</br>"persons" table create successfuly';
+		// else echo '</br>"persons" table not created beacuse of: '.mysql_error();
 	
-	/*----select from table----*/
-	// mysql_select_db('phpClass',$config);
-	// $resule=mysql_query('SELECT * FROM persons');
-	// while ($row=mysql_fetch_array($resule)) {
-	// 	echo '</br>'.$row['firstname'].' '.$row['lastname'].' is '.$row['age'].' years old';
-	// }
+	
+
 	}
 
-mysql_close($config);
+
 
  ?>
