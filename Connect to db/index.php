cfca357f7b58ace8291300ee1a9c1d0ff7e34121
mahlad <mahlad365@gmail.com>
2013-01-15 12:33:37 +0330

    seprate parts

--- Connect to db/index.php
+++ Connect to db/index.php
@@ -4,7 +4,14 @@
 </head>
 <body>
 <?php 
-include 'config.php';
+	/*----connect to db and create table----*/
+	include 'config.php';
+
+	/*----insert into table-just one time run----*/
+	//include 'insert_db.php';
+	
+	/*----select from table----*/
+	include 'select_db.php';
  ?>
 </body>
 </html>
